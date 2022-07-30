24d9831f2b560645dcf7972392aecfeb75b635c1
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-07-31 02:07:14 +0900

    fix: username req.param 검증 작업 추가 (#14)

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import validate from "../middlewares/param.validate";
 import user from "../controllers/userController";
 
 const router = Router();
 
-router.get("/:username", validate, user.userProfile);
-router.put("/:username", body('level').exists(), body('address').exists(), validate, user.userEdit);
+router.get("/:username", param('username').exists(), validate, user.userProfile);
+router.put("/:username", param('username').exists(), body('level').exists(), body('address').exists(), validate, user.userEdit);
 
 export default router;
