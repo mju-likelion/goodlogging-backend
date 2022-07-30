82700a9c54ba5a0704e0ebaf5cc42341f3d407c9
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-07-30 18:30:26 +0900

    feat: 유저 정보 편집 (이메일, 동네) (#6)

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
-import {Router} from "express";
+import { Router } from "express";
 import authRouter from "./authRouter";
 import challengeRouter from "./challengeRouter";
+import userRouter from "./userRouter";
 import auth from "../middlewares/auth";
 import validate from "../middlewares/param.validate";
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.use("/auth", authRouter);
 router.use('/challenge', auth, validate, challengeRouter);
+router.use("/user", auth, validate, userRouter);
 
 export default router;
---
+++ src/routers/userRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import validate from "../middlewares/param.validate";
+import user from "../controllers/userController";
+
+const router = Router();
+
+router.put("/:username", body('level').exists(), body('address').exists(), validate, user.userEdit);
+
+export default router;
