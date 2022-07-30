a1ada8b6ef1929e1cc15245adf29e0cf917c4ed6
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-07-31 01:41:29 +0900

    fix: auth 미들웨어 파일명 변경 (#12)

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { body } from "express-validator";
 import validate from "../middlewares/param.validate";
 import user from "../controllers/userController";
+import auth from "../controllers/authController";
 
 const router = Router();
 
-router.post("/register", body('username').exists(), body('email').exists(), body('password').exists(), body('level').exists(), body('address').exists(), validate, user.register);
-router.post("/login", body('email').exists(), body('password').exists(), validate, user.login);
+router.post("/register", body('username').exists(), body('email').exists(), body('password').exists(), body('level').exists(), body('address').exists(), validate, auth.register);
+router.post("/login", body('email').exists(), body('password').exists(), validate, auth.login);
 
 export default router;
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import authRouter from "./authRouter";
 import challengeRouter from "./challengeRouter";
 import userRouter from "./userRouter";
-import auth from "../middlewares/auth";
+import authValidate from "../middlewares/authValidate";
 import validate from "../middlewares/param.validate";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use('/challenge', auth, validate, challengeRouter);
-router.use("/user", auth, validate, userRouter);
+router.use('/challenge', authValidate, validate, challengeRouter);
+router.use("/user", authValidate, validate, userRouter);
 
 export default router;
