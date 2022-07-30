7a35c6e692916740037dc6cb05716313a0618d25
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-07-31 01:59:26 +0900

    feat: username 가진 유저 존재 검증 미들웨어 추가

---
+++ src/middlewares/getTargetUser.js
@@ -0,0 +1,17 @@
+import { User } from "../../models";
+import asyncWrapper from "../errors/wrapper";
+
+const userExistMiddleware = async (username) => {
+    const targetUser = await User.findOne({
+        where: {
+            username
+        }
+    });
+    if(!targetUser){
+        throw new APIError(httpStatus.BAD_REQUEST, errorCodes.USER_NOT_FOUND);
+    }
+};
+
+const isTargetUserExist = asyncWrapper(userExistMiddleware);
+
+export default isTargetUserExist;
