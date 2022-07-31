ff5ac72044cf9dc9cfa59b8863896d792989297d
강현욱 <rkdgusdnr32@naver.com>
2022-07-31 23:35:27 +0900

    feat: Plogging-Trash Model relation

--- models/Plogging.js
+++ models/Plogging.js
@@ -1,40 +1,41 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-module.exports = class Plogging extends Sequelize.Model{
-    static init(sequelize){
+module.exports = class Plogging extends Sequelize.Model {
+  static init(sequelize) {
+    {
+      return super.init(
         {
-           
-        return super.init({
-            owner:{
-                type: Sequelize.STRING(20),
-                allowNull:false,
-                unique:true,
-            },
-            date:{
-                type: 'TIMESTAMP',
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-            },
-            duration:{
-                type: Sequelize.INTEGER,
-                allowNull: false,
-            },
-            trash: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-            },
-            
-		}, {
-            sequelize,
-            timestamps: true,
-            modelName: 'Plogging',
-            tableName: 'ploggings',
-            paranoid: false,
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci',
-        });
+          owner: {
+            type: Sequelize.STRING(20),
+            allowNull: false,
+            unique: true,
+          },
+          date: {
+            type: 'TIMESTAMP',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          },
+          duration: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+          },
+        },
+        {
+          sequelize,
+          timestamps: true,
+          modelName: 'Plogging',
+          tableName: 'ploggings',
+          paranoid: false,
+          charset: 'utf8mb4',
+          collate: 'utf8mb4_general_ci',
+        }
+      );
     }
-}
-    static associate(db){
-        db.Plogging.belongsTo(db.User, {foreignKey:'owner' , targetKey:'username'})   
-    }
+  }
+  static associate(db) {
+    db.Plogging.hasMany(db.Trash);
+    db.Plogging.belongsTo(db.User, {
+      foreignKey: 'owner',
+      targetKey: 'username',
+    });
+  }
 };
--- models/Trash.js
+++ models/Trash.js
@@ -1,43 +1,46 @@
 const Sequelize = require('sequelize');
 
-module.exports = class Trash extends Sequelize.Model{
-    static init(sequelize){
-        return super.init({
-            //사용자, 사용자 위치, 해당되는 플로깅 모델, 날짜
-            owner: {
-                type: Sequelize.STRING(20),
-                allowNull: false,
-                unique: true
-            },
-            latitude: {
-                type: Sequelize.STRING(20),
-                allowNull: false,
-                unique: false
-            },
-            longitude:{
-                type: Sequelize.STRING(20),
-                allowNull: false,
-                unique: false
-            },
-            plogging:{
-                type: Sequelize.STRING(40),
-                allowNull: false,
-                unique: true
-            }
-
-
-		}, {
-            sequelize,
-            timestamps: true,
-            modelName: 'Trash',
-            tableName: 'trashes',
-            paranoid: false,
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci',
-        });
-    }
-    static associate(db){
-        db.Trash.hasOne(db.Plogging);
-       
-    }
+module.exports = class Trash extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        //사용자, 사용자 위치, 해당되는 플로깅 모델, 날짜
+        owner: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        latitude: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: false,
+        },
+        longitude: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: false,
+        },
+        plogging: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: 'Trash',
+        tableName: 'trashes',
+        paranoid: false,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+      }
+    );
+  }
+  static associate(db) {
+    db.Plogging.belongsTo(db.Plogging, {
+      foreignKey: 'plogging',
+      targetKey: 'date',
+    });
+  }
 };
