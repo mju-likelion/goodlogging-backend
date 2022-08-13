64ad6f45f844a04b1796b1db0d7bcc9151547d08
강현욱 <rkdgusdnr32@naver.com>
2022-08-14 02:34:11 +0900

    feat/pm2

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion**
 yarn install
 npm install pm2 -g
 pm2 start npm -- run start --prefix goodlogging-backend
