83395af4d0117ee7c37931cd645bb79f00fc10d2
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-08-14 22:06:16 +0900

    fix: 유저 프로필 수정 작업

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,19 +1,14 @@
 import { Router } from 'express';
-import { body, param } from 'express-validator';
+import { body } from 'express-validator';
 import validate from '../middlewares/param.validate';
 import user from '../controllers/userController';
 
 const router = Router();
 
-router.get(
-  '/:username',
-  param('username').exists(),
-  validate,
-  user.userProfile
-);
+router.get('/', user.userProfile);
+
 router.put(
-  '/:username',
-  param('username').exists(),
+  '/',
   body('level').exists(),
   body('address').exists(),
   validate,
