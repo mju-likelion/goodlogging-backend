113a05058c45be4f867a04af4dd5ccbb87c75c0d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-08-14 13:20:45 +0900

    test: 테스트 페이지 작성

--- src/routers/index.js
+++ src/routers/index.js
@@ -14,8 +14,13 @@ router.use('/challenge', authValidate, validate, challengeRouter);
 router.use('/user', authValidate, validate, userRouter);
 router.use('/plogging', authValidate, ploggingRouter);
 router.use('/upload', authValidate, uploadRouter);
+
 router.get('/', (req, res) => {
   return res.send('hello world!');
+});
+
+router.get('/test', (req, res) => {
+  return res.send('깃허브 변경 내역 저장 후 배포되었는지 테스트');
 });
 
 export default router;
