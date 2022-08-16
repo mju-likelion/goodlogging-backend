286aab933a6863da53e84904963de32d6cf81ced
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-08-16 22:21:47 +0900

    refactoring: mainRouter 리팩터링

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import validate from '../middlewares/param.validate';
 import main from '../controllers/mainController';
 
 const router = Router();
 
-router.get('/', validate, main.getMaininfo);
+router.get('/', main.getMaininfo);
 
 export default router;
