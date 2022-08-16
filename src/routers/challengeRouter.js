4fd6f5866bf062f94b58e7cfb9db0c301bc88d4b
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-08-17 01:53:50 +0900

    feat: 챌린지 조회 방식 수정

--- src/routers/challengeRouter.js
+++ src/routers/challengeRouter.js
@@ -1,17 +1,9 @@
 import challenge from '../controllers/challengeController';
 import { Router } from 'express';
-import { query } from 'express-validator';
-import validate from '../middlewares/param.validate';
 
 const router = Router();
 
-router.get(
-  '/',
-  query('year').exists(),
-  query('month').exists(),
-  validate,
-  challenge.getChallenge
-);
+router.get('/', challenge.getChallenge);
 router.put('/', challenge.editChallenge);
 
 export default router;
