93e0edc6a286d9c10a2b6242aaf046a2a79d660b
강현욱 <rkdgusdnr32@naver.com>
2022-08-19 21:04:24 +0900

    fix : cors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://develop--goodlogging.netlify.app/login',
+      'https://develop--goodlogging.netlify.app',
     ],
   })
 );
