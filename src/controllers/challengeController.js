d681a9eba994f0ed8b97772854b14db86b2b6cb6
강현욱 <rkdgusdnr32@naver.com>
2022-08-19 19:41:25 +0900

    fix : pm2

--- src/controllers/challengeController.js
+++ src/controllers/challengeController.js
@@ -14,18 +14,31 @@ const getChallenge = async (req, res) => {
 
   return res.json(challenge);
 };
+/////
 
-const editChallenge = (req, res) => {
-  const { user } = req;
-  const file = req.file;
+const editChallengeImage = async (req, res) => {
+  const {
+    params: { id },
+    file,
+    user,
+  } = req;
+  console.log('this is req.user: ' + req.user);
+  console.log('this is user.id: ' + user.id);
+  const isChallengeCorrect = await Challenge.findOne({
+    where: {
+      id: user.id,
+    },
+  });
 
-  console.log(user);
-  console.log(file);
-  const uploadedfile = upload.uploadImage(file, user);
-  console.log(uploadedfile);
+  if (!isChallengeCorrect) {
+    throw new APIError(httpStatus.BAD_REQUEST, errorCodes.BOARD_BAD_REQUEST);
+  }
+
+  const result = await uploadFunction(file, user, id, 'challenge');
+  return res.json(result);
 };
 
 export default {
   getChallenge: asyncWrapper(getChallenge),
-  editChallenge: asyncWrapper(editChallenge),
+  editChallenge: asyncWrapper(editChallengeImage),
 };
