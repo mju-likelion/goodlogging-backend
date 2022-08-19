50112719959b92e9b8ddb84f9b01ea60eec5a6e0
강현욱 <rkdgusdnr32@naver.com>
2022-08-19 21:47:49 +0900

    fix : feedRouter

--- src/routers/feedRouter.js
+++ src/routers/feedRouter.js
@@ -1,8 +1,8 @@
 import feed from '../controllers/feedController';
 import { query } from 'express-validator';
 import validate from '../middlewares/param.validate';
+import { Router } from 'express';
 
-const { Router } = require('express');
 const router = Router();
 
 router.get('/', query('sorted').exists(), validate, feed.mainFeed);
