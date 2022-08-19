24efc7fe4dea1f43aad9c6189aeb0d9686c43e23
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-08-19 23:22:12 +0900

    feat: 유저 정렬 시 추가적인 정보 제공 기능 추가

--- src/functions/userSort.js
+++ src/functions/userSort.js
@@ -2,16 +2,23 @@ import User from '../../models/User';
 
 const userSort = async (arr, user, sort) => {
   if (user != undefined) {
-    (
-      await User.findAll({
-        raw: true,
-        where: {
-          address: user.address,
-        },
-        order: [[`${sort}`, 'desc']],
-        attributes: ['username'],
-      })
-    ).forEach((user) => arr.push(user));
+    const sortResult = await User.findAll({
+      raw: true,
+      where: {
+        address: user.address,
+      },
+      order: [[`${sort}`, 'desc']],
+      attributes: ['username', 'trash', 'plogging'],
+    });
+    if (sort === 'plogging') {
+      sortResult.forEach((user) => {
+        arr.push({ username: user.username, plogging: user.plogging });
+      });
+    } else {
+      sortResult.forEach((user) => {
+        arr.push({ username: user.username, trash: user.trash });
+      });
+    }
   }
 };
 
