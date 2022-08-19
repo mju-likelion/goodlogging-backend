814a8b5e9abd1e0620fa74b0cd9203541ffec070
강현욱 <rkdgusdnr32@naver.com>
2022-08-19 20:55:09 +0900

    feat : ChallengeEdit

--- src/routers/challengeRouter.js
+++ src/routers/challengeRouter.js
@@ -1,9 +1,16 @@
 import challenge from '../controllers/challengeController';
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../../config/multer.config';
+import { param } from 'express-validator';
 
 const router = Router();
 
 router.get('/', challenge.getChallenge);
-router.put('/', challenge.editChallenge);
-
+router.put(
+  '/:id',
+  param('id').exists(),
+  multer(multerConfig).single('image'),
+  challenge.editChallengeImage
+);
 export default router;
